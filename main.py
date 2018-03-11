3e51124b80b1df4188f7c76a01b2f1dc46bd96e1
jeevan17 <jeevan17>
2018-03-11 11:07:08 +0530

    graph generated

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ import urllib2
 from bs4 import BeautifulSoup
 import re
 
+
 #quote_page = 'http://www.imdb.com/search/title?title_type=tv_series&sort=user_rating,desc'
 quote_page = 'http://www.imdb.com/search/title?title_type=tv_series&sort=user_rating,desc&page=1&ref_=adv_nxt'
 #quote_page = 'http://www.imdb.com/search/title?title_type=tv_series&sort=user_rating,desc&page=3&ref_=adv_nxt'
-
+title = 'http://www.imdb.com'
 page = urllib2.urlopen(quote_page)
 
 soup = BeautifulSoup(page, 'html.parser')
@@ -15,11 +16,27 @@ soup = BeautifulSoup(page, 'html.parser')
 name_box = soup.find_all('a', attrs={'href' : re.compile("_tt$")})
 
 count = 0
-#link=[]
+url=[]
 for name in name_box:
-	#link.append(name.get('href'))
-	print name.get('href')
-	count+=1
+	url.append(name.get('href'))
+	#print name.get('href')
+	#count+=1
 #name = name_box.text.strip()
-print count
+#print url
 #print name_box
+#print title+url[0]
+temp = {}
+for link in url:
+	quote_page = title+link
+	page = urllib2.urlopen(quote_page)
+	soup = BeautifulSoup(page, 'html.parser')
+	name = soup.find('h1', attrs={'itemprop' : 'name'})
+	name = name.string
+	rating = soup.find('span', attrs={'itemprop' : 'ratingValue'})
+	rating = rating.string
+	votes = soup.find('span', attrs={'itemprop' : 'ratingCount'})
+	votes = votes.string
+	print name+"  :"+rating+"  "+votes
+	temp[name] = [rating,votes]
+	#print temp
+print temp
---
+++ temp.py
@@ -0,0 +1,3 @@
+import matplotlib.pyplot as plt
+plt.plot([10,10,10,9.9,9.9,9.8,9.8,9.8],[5,6,10,12564,7,3515,29350,353],'ro')
+plt.show()
